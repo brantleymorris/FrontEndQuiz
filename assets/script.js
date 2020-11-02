61f11e7ee6f0717521c0aac96d761ace12671395
BPM <brantleymorris@me.com>
2020-11-02 16:37:07 -0600

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// To do list
+    // be sure to set variable for writing to html at top
+    // create array of objects for questions, probably need like 10
+
+    var questions = [
+        q1 = {
+            question = "Which of following can be used to add style to a web page?",
+            answer1 = "html",
+            answer2 = "css",
+            answer3 = "javascript",
+            answer4 = "all of the above",
+            correct = answer4 // need to make sure this will work right
+        },
+        q2 = {
+            question = "What does CSS stand for?",
+            answer1 = "Cyclical Styleing System",
+            answer2 = "Cicadian System Standards",
+            answer3 = "Cassading Style Sheets",
+            answer4 = "None of the above",
+            correct = answer3
+        }
+    ];
+    // create array of objecst for usernames and scores
+    // create listener to activate function
+    // create function to select and write questions (randomly from array, and remove from array when answers/ optional)
+    // create function to store usernames and scores to local storage
+    // write function for timer
