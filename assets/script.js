bb6a016e19ac5339d428bff8441ee24598c97139
BPM <brantleymorris@me.com>
2020-11-05 15:42:09 -0600

    added eventListeners

--- assets/script.js
+++ assets/script.js
@@ -3,18 +3,20 @@
     var questionEl = document.getElementById("questions");
     var timerEl = document.getElementById("timer");
     var scoresEl = document.getElementById("scores");
+    var begin= document.getElementById("startQuiz")
+    var currentQuestionIndex = 0;
     var user;
 
     // create array of objects for questions, probably need like 5
 
     var questions = [
-        q1 = {
+        q1 = { // remove q# for all objects
             question : "Which of following can be used to add style to a web page?",
             answer1 : "html",
             answer2 : "css",
             answer3 : "javascript",
             answer4 : "all of the above",
-            //correct : answer4  need to make sure this will work right
+            correct : "css"  // answer needs to match text
         },
         q2 = {
             question : "What does CSS stand for?",
@@ -22,7 +24,7 @@
             answer2 : "Cicadian System Standards",
             answer3 : "Cassading Style Sheets",
             answer4 : "None of the above",
-            //correct : answer3  need to make sure this will work correctly
+            correct : "Cassading Style Sheets"  //need to make sure this will work correctly
         },
         q3 = {
             question : "What is an API?",
@@ -51,31 +53,32 @@
     ];
     // create array of objects for usernames and scores
 
-    // create listener to activate function
-
     // create function to select and write questions (randomly from array, and remove from array when answers/ optional)
-    function writeQuesiton (i) {
-            var questionPrompt = document.createElement("ul");
-            questionPrompt.textContent = questions[i].question;
-            questionEl.append(questionPrompt);
+    function writeQuestion (i) {
+        var questionPrompt = document.createElement("ul");
+        questionPrompt.textContent = questions[i].question;
+        questionEl.append(questionPrompt);
 
-            var answerA = document.createElement("li");
-            answerA.textContent = questions[i].answer1;
-            questionPrompt.append(answerA);
+        var answerA = document.createElement("li");
+        answerA.textContent = questions[i].answer1;
+        answerA.setAttribute("id","0");
+        questionPrompt.append(answerA);
 
-            var answerB = document.createElement("li");
-            answerB.textContent = questions[i].answer2
-            questionPrompt.append(answerB);
+        var answerB = document.createElement("li");
+        answerB.textContent = questions[i].answer2
+        answerB.classList.add("1");
+        questionPrompt.append(answerB);
 
-            var answerC = document.createElement("li");
-            answerC.textContent = questions[i].answer3
-            questionPrompt.append(answerC);
+        var answerC = document.createElement("li");
+        answerC.textContent = questions[i].answer3
+        answerC.classList.add("2");
+        questionPrompt.append(answerC);
 
-            var answerD = document.createElement("li");
-            answerD.textContent = questions[i].answer4
-            questionPrompt.append(answerD);
+        var answerD = document.createElement("li");
+        answerD.textContent = questions[i].answer4
+        answerD.classList.add("3");
+        questionPrompt.append(answerD);
     };
-    // create function to store usernames and scores to local storage
 
     // write function for timer to be trigger by listener event when begin is pressed
     var secondsLeft = 60;
@@ -87,7 +90,7 @@
 
             if (secondsLeft === 0) {
                 clearInterval(timerInterval);
-                endMessage(); // need to write function to display score and say game is over
+                endMessage();
             }
         }, 1000);
     }
@@ -96,5 +99,26 @@
         timerEl.textContent = "Time is up"
     }
 
-setTime();
-writeQuesiton(0);
+// create function to store username and score to localStorage
+
+// add eventListener to setTime and writeQuetion
+begin.addEventListener("click", function() {
+    setTime();
+    writeQuestion(currentQuestionIndex);
+});
+
+//for (let i = 0; i < 4; i++) {
+    document.getElementById("0").addEventListener("click", function() {
+        currentQuestionIndex++;
+        if (document.getElementById("0").textContent === questions[currentQuestionIndex].correct) {
+            writeQuestion(currentQuestionIndex);
+            alert("Correct");
+            //userScore++;
+        }
+        else {
+            writeQuestion(currentQuestionIndex);
+            secondsLeft -= 10;
+            alert("Wrong");
+        }
+    })
+//}
