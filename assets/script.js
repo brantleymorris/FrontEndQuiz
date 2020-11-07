67bb740e5b76e8c748099f4096c8680b594e3cfa
BPM <brantleymorris@me.com>
2020-11-06 20:00:01 -0600

    added questions 3-5

--- assets/script.js
+++ assets/script.js
@@ -16,39 +16,39 @@
             answer2 : "css",
             answer3 : "javascript",
             answer4 : "all of the above",
-            correct : "css"  // answer needs to match text
+            correct : "css"
         },
         q2 = {
             question : "What does CSS stand for?",
             answer1 : "Cyclical Styling System",
             answer2 : "Cicadian System Standards",
-            answer3 : "Cassading Style Sheets",
+            answer3 : "Cascading Style Sheets",
             answer4 : "None of the above",
-            correct : "Cassading Style Sheets"  //need to make sure this will work correctly
+            correct : "Cascading Style Sheets"
         },
         q3 = {
             question : "What is an API?",
-            answer1 : "",
-            answer2 : "",
-            answer3 : "",
-            answer4 : "",
-            //correct :   need to make sure this will work correctly
+            answer1 : "All Participant Interface",
+            answer2 : "Application Programming Interface",
+            answer3 : "Applicable Program Installation",
+            answer4 : "All Program Issue",
+            correct : "Application Programming Interface"  
         },
         q4 = {
-            question : "",
-            answer1 : "",
-            answer2 : "",
-            answer3 : "",
-            answer4 : "",
-            //correct :  need to make sure this will work correctly 
+            question : "The document object model is also know as?",
+            answer1 : "DOM",
+            answer2 : "HTML",
+            answer3 : "API",
+            answer4 : "Javascript",
+            correct : "DOM" 
         },
         q5 = {
-            question : "",
-            answer1 : "",
-            answer2 : "",
-            answer3 : "",
-            answer4 : "",
-            //correct :  need to make sure this will work correctly
+            question : "Which of the following is used to add user functionality to a web page?",
+            answer1 : "html",
+            answer2 : "css",
+            answer3 : "javascript",
+            answer4 : "all of the above",
+            correct : "javascript"
         }
     ];
     // create array of objects for usernames and scores
@@ -61,23 +61,26 @@
 
         var answerA = document.createElement("li");
         answerA.textContent = questions[i].answer1;
-        answerA.setAttribute("id","0");
+        answerA.setAttribute("id","selectedA"); // need to figure out how to set this so that it can be called later
         questionPrompt.append(answerA);
 
         var answerB = document.createElement("li");
         answerB.textContent = questions[i].answer2
-        answerB.classList.add("1");
+        answerB.setAttribute("id", "selectedB")
         questionPrompt.append(answerB);
 
         var answerC = document.createElement("li");
         answerC.textContent = questions[i].answer3
-        answerC.classList.add("2");
+        answerC.setAttribute("id", "selectedC")
         questionPrompt.append(answerC);
 
         var answerD = document.createElement("li");
         answerD.textContent = questions[i].answer4
-        answerD.classList.add("3");
+        answerD.setAttribute("id", "selectedD")
         questionPrompt.append(answerD);
+
+        // need to add something to remove .show, changes it to hide and will need to set display: hide; in css
+        document.querySelector(".show").setAttribute("class", "hide");
     };
 
     // write function for timer to be trigger by listener event when begin is pressed
@@ -99,26 +102,25 @@
         timerEl.textContent = "Time is up"
     }
 
-// create function to store username and score to localStorage
+// create function to prompt username and store to localStorage
 
-// add eventListener to setTime and writeQuetion
-begin.addEventListener("click", function() {
-    setTime();
-    writeQuestion(currentQuestionIndex);
-});
+// eventListener to setTime and writeQuetion
+    begin.addEventListener("click", function() {
+        setTime();
+        writeQuestion(currentQuestionIndex);
+    });
 
-//for (let i = 0; i < 4; i++) {
-    document.getElementById("0").addEventListener("click", function() {
-        currentQuestionIndex++;
-        if (document.getElementById("0").textContent === questions[currentQuestionIndex].correct) {
-            writeQuestion(currentQuestionIndex);
-            alert("Correct");
-            //userScore++;
+    document.getElementById("answerA").addEventListener("click", function() { // not selecting properly, may need to set variable for target before adding listener?
+        if (document.getElementById("answerA").textContent === questions[currentQuestionIndex].correct) {
+            currentQuestionIndex ++;
+            alert("Correct"); // may not need this, will just get in the way
+            secondsLeft += 10; //use secondsLeft for userscore?
+            writeQuestions(currentQuestionIndex);
         }
         else {
-            writeQuestion(currentQuestionIndex);
+            currentQuestionIndex ++;
             secondsLeft -= 10;
             alert("Wrong");
+            writeQuestion(currentQuestionIndex);
         }
-    })
-//}
+    });
